b5db9aebeb4a17c4ffa894e4bb4c5221fa949f82
alexeymo <>
2018-03-07 02:37:05 +0300

    add loops

--- input.tf
+++ input.tf
@@ -1,21 +1,5 @@
 /* Common depedecies */
-variable "alias" {}
 variable "region" {}
 variable "accountId" {}
 
-# variable "apiParameters" { type = "list" } # add example
-
-/* Lambda depedecies */
-variable "lambda_publish" {}
-variable "lambda_source_name" {}
-variable "lambda_function_name" {}
-
-/* API Gateway depedecies */
-variable "api_gateway_name" {}
-variable "api_gateway_description" {}
-variable "api_gateway_version" {}
-variable "api_gateway_path" {}
-variable "api_gateway_method" {}
-variable "api_gateway_type" {}
-variable "api_gateway_dataType_input" {}
-variable "api_gateway_dataType_output" {}
+variable "dataStructure" { type = "list" }
--- main.tf
+++ main.tf
@@ -5,54 +5,61 @@ module "lambda-default-iam" {
 }
 
 resource "aws_lambda_function" "function" {
-  s3_bucket         = "sche-mcc-infra"
-  s3_key            = "builds/${var.lambda_source_name}"
-  function_name     = "${var.lambda_function_name}"
+  count             = "${length(var.dataStructure)}"
+  s3_bucket         = "${lookup(var.dataStructure[count.index],"lambda_function_s3_bucket")}"
+  s3_key            = "${lookup(var.dataStructure[count.index],"lambda_function_source_path")}/${lookup(var.dataStructure[count.index],"lambda_function_source_name")}"
+  function_name     = "${lookup(var.dataStructure[count.index],"lambda_function_name")}"
   role              = "${module.lambda-default-iam.lambda_role_arn}"
-  handler           = "script.lambda_handler"
-  runtime           = "python2.7"
-  publish           = "${var.lambda_publish}"
+  handler           = "${lookup(var.dataStructure[count.index],"lambda_function_handler")}"
+  runtime           = "${lookup(var.dataStructure[count.index],"lambda_function_runtime")}"
+  publish           = "True"
 }
 
 resource "aws_lambda_alias" "alias" {
-  name             = "${var.alias}"
-  function_name    = "${aws_lambda_function.function.arn}"
-  function_version = "${aws_lambda_function.function.version}"
+  count            = "${length(var.dataStructure)}"
+  name             = "${lookup(var.dataStructure[count.index],"alias")}"
+  function_name    = "${element(aws_lambda_function.function.*.arn, count.index)}"
+  function_version = "${element(aws_lambda_function.function.*.version, count.index)}"
 }
 
 data "template_file" "swagger_api" {
-  template = "${file("swagger_api.yml")}"
+  count    = "${length(var.dataStructure)}"
+  template = "${file(lookup(var.dataStructure[count.index],"api_gateway_swagger_template_path"))}"
 
   vars {
-    api_gateway_name = "${var.api_gateway_name}"
-    api_gateway_description = "${var.api_gateway_description}"
-    api_gateway_version = "${var.api_gateway_version}"
-    api_gateway_endpoint_uri = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${aws_lambda_alias.alias.arn}/invocations"
-    api_gateway_path = "${var.api_gateway_path}"
-    api_gateway_method = "${var.api_gateway_method}"
-    api_gateway_type = "${var.api_gateway_type}"
-    api_gateway_dataType_input = "${var.api_gateway_dataType_input}"
-    api_gateway_dataType_output = "${var.api_gateway_dataType_output}"
+    api_gateway_name = "${lookup(var.dataStructure[count.index],"api_gateway_name")}"
+    api_gateway_description = "${lookup(var.dataStructure[count.index],"api_gateway_description")}"
+    api_gateway_version = "${lookup(var.dataStructure[count.index],"api_gateway_version")}"
+    api_gateway_endpoint_uri = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${element(aws_lambda_alias.alias.*.arn, count.index)}/invocations"
+    api_gateway_path = "${lookup(var.dataStructure[count.index],"api_gateway_path")}"
+    api_gateway_method = "${lookup(var.dataStructure[count.index],"api_gateway_method")}"
+    api_gateway_type = "${lookup(var.dataStructure[count.index],"api_gateway_type")}"
+    api_gateway_dataType_input = "${lookup(var.dataStructure[count.index],"api_gateway_dataType_input")}"
+    api_gateway_dataType_output = "${lookup(var.dataStructure[count.index],"api_gateway_dataType_output")}"
   }
 }
 
 resource "aws_api_gateway_rest_api" "RootAPI" {
-  name        = "API NAME" # "${var.api_gateway_name}"
-  description = "api description" # "${var.api_gateway_description}"
-  body        = "${data.template_file.swagger_api.rendered}"
+  count       = "${length(var.dataStructure)}"
+  name        = "${lookup(var.dataStructure[count.index],"api_gateway_name")}"
+  description = "${lookup(var.dataStructure[count.index],"api_gateway_description")}"
+  body        = "${element(data.template_file.swagger_api.*.rendered, count.index)}"
 }
 
 resource "aws_lambda_permission" "allow_api_gateway" {
-  function_name = "${aws_lambda_function.function.arn}"
+  depends_on    = ["aws_api_gateway_rest_api.RootAPI"]
+  count         = "${length(var.dataStructure)}"
+  function_name = "${element(aws_lambda_function.function.*.arn, count.index)}"
   statement_id  = "AllowExecutionFromApiGateway"
   action        = "lambda:InvokeFunction"
   principal     = "apigateway.amazonaws.com"
-  source_arn    = "arn:aws:execute-api:${var.region}:${var.accountId}:${aws_api_gateway_rest_api.RootAPI.id}/*/*/*"
-  qualifier     = "${aws_lambda_alias.alias.name}"
+  source_arn    = "arn:aws:execute-api:${var.region}:${var.accountId}:${element(aws_api_gateway_rest_api.RootAPI.*.id, count.index)}/*/${upper(lookup(var.dataStructure[count.index],"api_gateway_method"))}/${lookup(var.dataStructure[count.index],"api_gateway_path")}"
+  qualifier     = "${element(aws_lambda_alias.alias.*.name, count.index)}"
 }
 
 resource "aws_api_gateway_deployment" "DeployAPI" {
-  rest_api_id = "${aws_api_gateway_rest_api.RootAPI.id}"
-  description = "Targets to ${aws_lambda_function.function.arn}:${aws_lambda_alias.alias.name}"
-  stage_name  = "${aws_lambda_alias.alias.name}"
+  count       = "${length(var.dataStructure)}"
+  rest_api_id = "${element(aws_api_gateway_rest_api.RootAPI.*.id, count.index)}"
+  description = "Targets to ${element(aws_lambda_function.function.*.arn, count.index)}:${element(aws_lambda_alias.alias.*.name, count.index)}"
+  stage_name  = "${element(aws_lambda_alias.alias.*.name, count.index)}"
 }
