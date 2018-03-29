851eff32a69dba643f8588663cbedec17035c994
alexeymo <>
2018-03-29 16:44:58 +0300

    add IAM role arn and id to output

--- output.tf
+++ output.tf
@@ -5,3 +5,11 @@ output "lambda_function_id" {
 output "apigw_id" {
   value = "${aws_api_gateway_rest_api.RootAPI.id}"
 }
+
+output "lambda_role_arn" {
+  value = "${module.lambda-default-iam.lambda_role_arn}"
+}
+
+output "lambda_role_id" {
+  value = "${module.lambda-default-iam.lambda_role_id}"
+}
